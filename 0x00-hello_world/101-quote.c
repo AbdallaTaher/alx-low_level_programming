2ae7c3d3e02497cd86d648b9fe3d54b8e7935156
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-11 23:50:52 +0200

    s

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+
+/**
+ * main - Entry
+ * function for printing without using printf
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	syscall(SYS_write, 1, msg, sizeof(msg));
+
+	return (1);
+}

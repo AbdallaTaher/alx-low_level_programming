1394d07816e0a3baeb9bfd5bcccba2265250bae6
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-11 22:40:55 +0200

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,10 @@
  * This function is main
  * and used to ptinting phrase
  * using puts
- * */
+ */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

fde20fb31719608775b8bf6356e1f49d949a75bc
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-12 22:24:19 +0200

    task_3 in C

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * This function to print alphapets in lowercase
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}

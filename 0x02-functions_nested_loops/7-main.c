97cb780b6e90d3cd9860b39f6caaa9f5ae1925ac
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-17 01:37:47 +0200

    s

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ * This function -> checks letters to be lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int r;
+
+	r = print_last_digit(98);
+	printf("%d\n", r);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit -> checks letters for lowercase
+ * @n: the test character
+ * Return: 1 Success
+ */
+
+int print_last_digit(int n)
+{
+	return (n % 10);
+}

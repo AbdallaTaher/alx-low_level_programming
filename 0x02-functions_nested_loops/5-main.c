547c63fff5db8e997fbc8fec0c48ea90de371a9e
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-17 00:47:42 +0200

    d

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * This function -> checks letters to be lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_sign(98);
+	/*
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');*/
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign -> checks letters for lowercase
+ * @n: the test character
+ * Return: 1 Success
+ */
+
+int Print_sign(int n)
+{
+	int test;
+	if (n > 0)
+	{
+		_putchar('+');
+		test = 1;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		test = 0;
+	}
+	else
+	{
+		_putchar('-');
+		test = -1;
+	}
+	return(test);
+}

f8ad9613c41163c137510e415161556a8f682a35
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-16 14:45:18 +0200

    f

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * This program to print _putchar
+ * Retun : Always 0 (Success)
+ */
+
+int main(void)
+{
+	char phrase[] = "_putchar";
+	for(int i = 0; i <= sizeof(phrase); i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

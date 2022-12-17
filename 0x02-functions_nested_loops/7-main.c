cecd036dd0dd51e338db142884f7df463610dbed
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-17 11:44:23 +0200

    s

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -11,7 +11,10 @@ int main(void)
 {
 	int r;
 
-	r = print_last_digit(98);
-	printf("%d\n", r);
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0'+r);
+	_putchar('\n');
 	return (0);
 }

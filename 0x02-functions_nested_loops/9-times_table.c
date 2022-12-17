7a47636889d1e0b03e17e853767f58d387cbe434
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-17 14:24:12 +0200

    Numbers times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table -> checks letters for lowercase
+ * Return: 1 Success
+ */
+
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if ((i * j) / 10)
+			{
+				_putchar(((i * j) / 10) + '0');
+			}
+			_putchar(((i * j) % 10) + '0');
+			if (j != 9)
+			{
+			_putchar(',');
+			}
+			else
+			{
+				continue;
+			}
+			if ((j * i) < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}

8ddaee0114035dea945fb4b9d0ddd6fb15e50213
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-17 22:34:37 +0200

    fizzbuzz game

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -23,13 +23,13 @@ int main(void)
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ")
+			printf("FizzBuzz ");
 		}
 		else
 		{
 			printf("i ");
 		}
-	{
+	}
 	printf("\n");
-	retrun(0);
+	return(0);
 }

a8ab36241eee466f59ec7ead59e7d4df26f0e660
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-17 20:34:49 +0200

    drawing line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line -> checks letters for lowercase
+ * @n: the number of times _ to print
+ * Return: 1 Success
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}

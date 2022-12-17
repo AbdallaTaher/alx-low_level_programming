9fea346b49c1c61bbdd7d3774e0f3532d04026d3
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-17 15:51:07 +0200

    printing numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 -> checks letters for lowercase
+ * @n: the start number
+ * Return: 1 Success
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d, ", n);
+	}
+}

a8f8e9f3e0f93c952328e4e1c626bab6165530f7
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-17 14:42:28 +0200

    Numbers times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,9 +18,9 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 			}
-			else if((i * j) >= 10)
+			else if ((i * j) >= 10)
 			{
-		  		_putchar(' ');
+				_putchar(' ');
 			}
 			if ((i * j) / 10)
 			{

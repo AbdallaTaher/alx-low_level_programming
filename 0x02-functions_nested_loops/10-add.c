2464ccdde162220529c22c5478a1c8c8584f76b2
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-17 14:55:03 +0200

    add2_nums

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add -> adds two numbers
+ * @n : the first number to be aded
+ * @j : the second number to be added
+ * Return: 1 Success
+ */
+
+int add(int n, int j)
+{
+	return (n + j);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,37 +5,7 @@
  * Return: 1 Success
  */
 
-void times_table(void)
+int add(int n, int j)
 {
-	int i, j;
-
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			if ((j * i) < 10 && j != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if ((i * j) >= 10)
-			{
-				_putchar(' ');
-			}
-			if ((i * j) / 10)
-			{
-				_putchar(((i * j) / 10) + '0');
-			}
-			_putchar(((i * j) % 10) + '0');
-			if (j != 9)
-			{
-			_putchar(',');
-			}
-			else
-			{
-				continue;
-			}
-		}
-		_putchar('\n');
-	}
+	return (n + j);
 }

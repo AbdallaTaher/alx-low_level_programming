c815b3ffcdc58a5bb679925810265a7e694bacc0
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-17 15:54:51 +0200

    printing numbers

---
+++ 0x02-functions_nested_loops/11-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * main - check the code.
+ * This function -> checks letters to be lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_to_98(0);
+	print_to_98(98);
+	print_to_98(111);
+	return (0);
+}

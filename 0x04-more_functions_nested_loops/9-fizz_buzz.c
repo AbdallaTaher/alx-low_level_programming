b65807b75c7a0217e0ade9f5a97a49a926e2bc61
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-17 22:30:10 +0200

    fizzbuzz game

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -> checks letters for lowercase
+ * size: the number of times _ to print
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz")
+		}
+		else
+		{
+			printf("i");
+		}
+	{
+	printf("\n");
+	retrun(0);
+}

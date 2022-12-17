219f05fe44374553a6e839580793d23f79c132c8
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-17 16:55:30 +0200

    uppercase check

---
+++ 0x04-more_functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower -> checks letters for lowercase
+ * @c: the test character
+ * Return: 1 Success
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}

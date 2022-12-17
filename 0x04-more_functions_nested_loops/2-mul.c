659c914faf372933a9e2e221726925c910ab0528
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-17 20:02:23 +0200

    multiblication of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul -> checks letters for lowercase
+ * @a: the first number to multibly
+ * @b: the second number to multibly
+ * Return: 1 Success
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

b171d1ec99e3525810552c07499b991b5211c7cb
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-18 23:22:12 +0200

    s

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,17 +8,17 @@
 
 int print_last_digit(int n)
 {
-	int x;
+	int a;
 
 	if (n < 0)
 	{
 		n = -n;
 	}
-	x = n % 10;
-	if (x < 0)
+	a = n % 10;
+	if (a < 0)
 	{
-		x = -x;
+		a = -a;
 	}
-	_putchar(x + '0');
-	return (x);
+	_putchar(a + '0');
+	return (a);
 }
---
+++ 0x02-functions_nested_loops/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main: dsf
+ * Return: number
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		printf("%c", i);
+	}
+	printf("\n");
+}

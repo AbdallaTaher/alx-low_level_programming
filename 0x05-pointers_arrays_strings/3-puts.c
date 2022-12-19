677e91c8eca3967870b3ad9bccee2eb86e2be30a
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-19 23:04:47 +0200

    printing string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts -> changinh value of number through pointers
+ * @str: variable
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		_putchar(i);
+	}
+}

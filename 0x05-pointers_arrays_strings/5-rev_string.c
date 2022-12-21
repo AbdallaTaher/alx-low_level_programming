3565dd3378f2a6c387b594d886b331ce36fd52be
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-21 21:00:24 +0200

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string -> changinh value of number through pointers
+ * @s: variable
+ */
+
+void rev_string(char *s)
+{
+	int a, i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	i--;
+	for (a = 0; j <= i; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}

1dfccfb6ccf408904a7bdf5097e33a57a310da4b
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-23 11:47:04 +0200

    print others in string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 -> changinh value of number through pointers
+ * @str: variable
+ */
+
+void puts2(char *str)
+{
+	int j, i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(s[i]);
+		}
+	}
+
+	_putchar('\n');
+}

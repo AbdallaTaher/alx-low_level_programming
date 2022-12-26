bd9ce16d93adfdaca072a13d82ce3a16ecf16272
Abdalla Taher <abdallataher2015@gmail.com>
2022-12-26 23:16:53 +0200

    s

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * largest_number - returns largest number
+ * @a: viriable
+ * @b: variable
+ * @c: variable
+ * Return : leagest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if(b > c && b > a)
+	{
+		largest = b;
+	}
+	else if (c > b)
+	{
+		largest = c;
+	}
+	else
+	{
+		largest = b;
+	}
+	return (largest); 
+}
